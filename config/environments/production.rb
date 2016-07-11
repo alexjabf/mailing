7071fc065c00d05fe396b7c2557185e7d4ab8245
Alejandro Ballesteros <alexjabf@hotmail.com>
2016-07-11 15:31:52 -0500

    sqlite3 gem removed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.active_support.deprecation = :stderr 
+  config.action_mailer.default_url_options = { :host => "#{ENV['APP_DOMAIN']}:#{ENV['APP_PORT']}" } 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default :charset => 'utf-8'
+  ActionMailer::Base.default from: "desafio.com"
+  ActionMailer::Base.smtp_settings = {
+    :address => ENV['SMTP_ADDRESS'],
+    :port => ENV['SMTP_PORT'],
+    :authentication => :login,
+    :domain => "gmail.com",
+    :user_name => ENV['EMAIL_ADDRESS'],
+    :password => ENV['EMAIL_PASSWORD']
+  }
+  
 end
